a160a3baaba4d5d302c8f6972dce2ad4488821ad
Sundars0 <Sundars04@rediffmail.com>
2016-10-29 08:01:29 +0000

    changes layout for block, page and section

--- app/admin/block.rb
+++ app/admin/block.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Block do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :title, :show_title, :body, :position, :display, :class_suffix, :order, :is_published
 #
 # or
 #
@@ -12,6 +12,31 @@ ActiveAdmin.register Block do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+  
+  index do
+    column :id
+    column :title, :sorted => :title
+    column :position, :sorted => :position
+    column :display, :sorted => :display
+    column :created_at, :sorted => :created_at
+    column :order
+    actions
+  end
 
+  form do |f|
+    f.inputs "Details" do
+      f.input :title, :label => 'Title'
+      f.input :show_title, :label => 'Show Title'      
+      f.input :body, :as => :html_editor, :label => 'Body'
+      f.input :position, :label => 'Position', :as => :select, 
+              :collection => [["Jumbotron", "jumbotron"], ["Block", "block"]]
+      f.input :display, :label => 'Display', :as => :select, 
+              :collection => [["Al Pages", "all"], ["Homepage Only", "home"], ["All But Homepage", "nohome"]]
+      f.input :order, :label => 'Order'
+      f.input :class_suffix, :lable => 'Class Suffix'
+      f.input :is_published, :label => 'Published'
+      f.actions
+    end
+  end
 
 end
--- app/admin/page.rb
+++ app/admin/page.rb
@@ -26,10 +26,10 @@ permit_params :title, :body, :order, :is_published, :section_id, :menu_display
     f.inputs "Details" do
       f.input :title, :label => 'Title'
       f.input :section, :label => 'Section'
-      f.input :body, :label => 'Body'
+      f.input :body, :as => :html_editor, :label => 'Body'
       f.input :order, :label => 'Order'
       f.input :is_published, :label => 'Published'
-      f.input :menu_display, :lable => 'Disply In Menu'
+      f.input :menu_display, :lable => 'Display In Menu'
       f.actions
     end
   end
