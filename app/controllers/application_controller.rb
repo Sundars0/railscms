0c421e8dde45f2261b890406f3b875bcd915a438
Sundars0 <Sundars04@rediffmail.com>
2016-10-29 13:28:38 +0000

    modified the fontpage layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
-  before_filter :getPageNav
+  before_filter :getPageNav, :getjumbotron, :getblocks
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   def getPageNav
     @pageNav = Page.where("menu_display = true && is_published = true").order(:order => :asc)    
+  end
+
+  def getjumbotron
+    @jumbotron = Block.where("position = 'jumbotron'")
+  end
+
+  def getblocks
+    @blocks = Block.where("position = 'block'")
   end
 end
